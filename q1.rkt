68f637c1fbb442050ce68a0e0bfc061606d90272
Sai-Puneeth <spuneeth987@gmail.com>
2019-11-18 22:29:16 +0530

    q2 and q1

---
+++ q1.rkt
@@ -0,0 +1,14 @@
+#lang racket
+(define (sumUpRecursive inputList)
+  (if (null? inputList)
+      0
+      (+ (first inputList) (sumUpRecursive (rest inputList)))
+     )
+  )
+
+(define (sumUpTailRecursion inputList listSum)
+  (if (null? inputList)
+      listSum
+      (sumUpTailRecursion (rest inputList) (+ (first inputList) listSum))
+      )
+  )
