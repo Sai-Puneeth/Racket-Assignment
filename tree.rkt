1a187ad38d559d4caf48c1185e69681f27cfede6
Sai-Puneeth <spuneeth987@gmail.com>
2019-11-18 19:10:56 +0530

    5th

--- tree.rkt
+++ tree.rkt
@@ -1,26 +1,11 @@
 #lang racket
- 
-(define the-tree ; Node: (list <data> <left> <right>)
-  '(1 (2 (4 (7 #f #f) #f) (5 #f #f)) (3 (6 (8 #f #f) (9 #f #f)) #f)))
- 
-(define (preorder tree visit)
-  (let loop ([t tree])
-    (when t (visit (car t)) (loop (cadr t)) (loop (caddr t)))))
-(define (inorder tree visit)
-  (let loop ([t tree])
-    (when t (loop (cadr t)) (visit (car t)) (loop (caddr t)))))
-(define (postorder tree visit)
-  (let loop ([t tree])
-    (when t (loop (cadr t)) (loop (caddr t)) (visit (car t)))))
-(define (levelorder tree visit)
-  (let loop ([trees (list tree)])
-    (unless (null? trees)
-      ((compose1 loop (curry filter values) append*)
-       (for/list ([t trees] #:when t) (visit (car t)) (cdr t))))))
- 
-(define (run order)
-  (printf "~a:" (object-name order))
-  (order the-tree (λ(x) (printf " ~s" x)))
-  (newline))
-(for-each run (list preorder inorder postorder levelorder))
- 
+(define emptyTree '())
+
+(define (addToTree Tree k v)
+  (match Tree
+    ['()
+     (list k v '() '())]
+    [(list key val left right)
+     (if (> k key)
+       (list key val left (addToTree right k v))
+       (list key val (addToTree left k v) right))]))
